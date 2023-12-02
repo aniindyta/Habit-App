51e527944bad95ecb2e4f89869b332e2b369dc1c
aniindyta <aninditaputri055@gmail.com>
2023-12-03 01:54:37 +0700

    show notification with pending intent

--- app/src/main/java/com/dicoding/habitapp/notification/NotificationWorker.kt
+++ app/src/main/java/com/dicoding/habitapp/notification/NotificationWorker.kt
@@ -1,11 +1,20 @@
 package com.dicoding.habitapp.notification
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.dicoding.habitapp.R
+import com.dicoding.habitapp.ui.add.AddHabitActivity
 import com.dicoding.habitapp.utils.HABIT_ID
 import com.dicoding.habitapp.utils.HABIT_TITLE
+import com.dicoding.habitapp.utils.NOTIFICATION_CHANNEL_ID
+import com.dicoding.habitapp.utils.NOTIF_UNIQUE_WORK
 
 class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
 
@@ -17,8 +26,44 @@ class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, p
         val shouldNotify = prefManager.getBoolean(applicationContext.getString(R.string.pref_key_notify), false)
 
         //TODO 12 : If notification preference on, show notification with pending intent
+        if (shouldNotify) {
+            val notificationManager = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
+            val notificationId = habitId
+
+            val notificationBuilder =
+                NotificationCompat.Builder(applicationContext, NOTIFICATION_CHANNEL_ID).apply {
+                    setSmallIcon(R.drawable.ic_notifications)
+                    setContentTitle(habitTitle)
+                    setContentText(applicationContext.getString(R.string.notify_content))
+
+                    val intent = Intent(applicationContext, AddHabitActivity::class.java)
+                    intent.putExtra(HABIT_ID, habitId) // Pass the habit ID to the AddHabitActivity
+                    intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    val pendingIntent = PendingIntent.getActivity(
+                        applicationContext,
+                        0,
+                        intent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                    )
+
+                    setContentIntent(pendingIntent)
+                    setAutoCancel(true)
+                }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                if (notificationManager.getNotificationChannel(NOTIFICATION_CHANNEL_ID) == null) {
+                    val channel = NotificationChannel(
+                        NOTIFICATION_CHANNEL_ID,
+                        NOTIF_UNIQUE_WORK,
+                        NotificationManager.IMPORTANCE_DEFAULT
+                    )
+                    notificationManager.createNotificationChannel(channel)
+                }
+            }
+
+            notificationManager.notify(notificationId, notificationBuilder.build())
+        }
         return Result.success()
     }
-
 }
