3dfdd5459724431194cca6c5733c23ae156b8edc
aniindyta <aninditaputri055@gmail.com>
2023-12-03 01:53:59 +0700

    update theme based on value in ListPreference

--- app/src/main/java/com/dicoding/habitapp/setting/SettingsActivity.kt
+++ app/src/main/java/com/dicoding/habitapp/setting/SettingsActivity.kt
@@ -7,8 +7,10 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 import com.dicoding.habitapp.R
+import com.dicoding.habitapp.utils.DarkMode
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -48,6 +50,19 @@ class SettingsActivity : AppCompatActivity() {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
             //TODO 11 : Update theme based on value in ListPreference
+            val switchDarkMode: ListPreference? = findPreference(getString(R.string.pref_key_dark))
+            switchDarkMode?.setOnPreferenceChangeListener { _, newValue ->
+                when (newValue.toString()) {
+                    getString(R.string.pref_dark_follow_system) -> {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+                            updateTheme(DarkMode.FOLLOW_SYSTEM.value)
+                        else updateTheme(DarkMode.ON.value)
+                    }
+                    getString(R.string.pref_dark_off) -> updateTheme(DarkMode.OFF.value)
+                    else -> updateTheme(DarkMode.ON.value)
+                }
+                true
+            }
         }
 
         private fun updateTheme(mode: Int): Boolean {
