1e1a48a38900a4d769120e3d3261c6f1526e3b17
aniindyta <aninditaputri055@gmail.com>
2023-12-03 02:03:16 +0700

    ui test the add habit button

--- app/src/androidTest/java/com/dicoding/habitapp/ui/list/HabitActivityTest.kt
+++ app/src/androidTest/java/com/dicoding/habitapp/ui/list/HabitActivityTest.kt
@@ -1,6 +1,24 @@
 package com.dicoding.habitapp.ui.list
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.dicoding.habitapp.R
+import org.junit.Rule
+import org.junit.Test
+
 //TODO 16 : Write UI test to validate when user tap Add Habit (+), the AddHabitActivity displayed
 class HabitActivityTest {
+    @get:Rule
+    val activityRule = ActivityScenarioRule(HabitListActivity::class.java)
 
+    @Test
+    fun addNewHabit() {
+        onView(withId(R.id.fab)).perform(click())
+        onView(withText("Add Habit")).check(matches(isDisplayed()))
+    }
 }
